60b4b9f7b463fd8b8628848dedb3e19cda3bb784
Sun Jieyan <procedureyan@outlook.com>
2022-08-29 19:15:03 +0800

    蛇的创建和移动

--- web/src/components/GameMap.vue
+++ web/src/components/GameMap.vue
@@ -1,20 +1,23 @@
 <template>
     <div ref="parent" class="gamemap">
-        <canvas ref="canvas"></canvas>
+        <canvas ref="canvas" tabindex="0"></canvas>
     </div>
 </template>
 
 <script>
-import {GameMap} from "@/assets/scripts/GameMap"
-import { ref, onMounted} from 'vue';
-export default{
-    setup(){
-        let parent=ref(null);
-        let canvas=ref(null);
-        onMounted(()=>{
-            new GameMap(canvas.value.getContext('2d'),parent.value);
+import { GameMap } from "@/assets/scripts/GameMap";
+import { ref, onMounted } from 'vue'
+
+export default {
+    setup() {
+        let parent = ref(null);
+        let canvas = ref(null);
+
+        onMounted(() => {
+            new GameMap(canvas.value.getContext('2d'), parent.value)
         });
-        return{
+
+        return {
             parent,
             canvas
         }
@@ -23,12 +26,11 @@ export default{
 </script>
 
 <style scoped>
-div.gamemap{
-    width:100%;
+div.gamemap {
+    width: 100%;
     height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
 }
-
 </style>
